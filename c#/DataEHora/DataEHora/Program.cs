e64e5f5261149d1ccf107ebc2f51ae46989c5595
rocha.dev <juniorbing0317@gmail.com>
2025-03-03 09:34:49 -0300

    Data e Hora

---
+++ c#/DataEHora/DataEHora/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DataEHora;
+
+class Program
+{
+    static void Main()
+    {
+        DateOnly dia = new DateOnly(2021, 12, 31);
+        string diaEmtexto = dia.ToLongDateString();
+
+        Console.WriteLine(diaEmtexto);
+
+        DateTime dataHora = new DateTime(2021, 12, 31, 23, 59, 59);
+        Console.WriteLine(dataHora);
+
+        DateTime hoje = DateTime.UtcNow;
+        Console.WriteLine(hoje);
+
+        DateTime novaData = dataHora.AddDays(1);
+        Console.WriteLine(novaData);
+    }
+}
