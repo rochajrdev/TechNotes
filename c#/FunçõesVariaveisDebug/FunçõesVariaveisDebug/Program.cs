f65b8ae03ac59133ac3fdf91eb7eb6aa4ed68112
rocha.dev <juniorbing0317@gmail.com>
2025-03-04 22:07:56 -0300

    funções

---
+++ c#/FunçõesVariaveisDebug/FunçõesVariaveisDebug/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FunçõesVariaveisDebug
+{
+    class Program
+    {
+        static void Main()
+        {
+            var matematica = new operacoesMatematicas();
+
+            //matematica.Adicionar(10, 20);
+            var resultado = matematica.Adicionar(valor1: 10, valor2: 20);
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
+
---
+++ c#/FunçõesVariaveisDebug/FunçõesVariaveisDebug/operacoesMatematicas.cs
@@ -0,0 +1,29 @@
+﻿
+namespace FunçõesVariaveisDebug;
+
+public class operacoesMatematicas
+{
+    /*public void Adicionar(int valor1, int valor2)
+    {
+        var resultado = valor1 + valor2;
+
+        Console.WriteLine(resultado);
+    }*/ 
+    
+    /*public int Adicionar(int valor1, int valor2) // Alterado para retornar um inteiro
+    { //o que essa função está fazendo é somar dois valores inteiros e retornar o resultado
+        return valor1 + valor2; // soma os valores e retorna o resultado
+    }*/
+    // outra forma:
+    //public int Adicionar(int valor1, int valor2) => valor1 + valor2; // soma os valores e retorna o resultado
+
+    // para retornar mais de um valor em uma função:
+    public (int, string) Adicionar(int valor1, int valor2)
+    {
+        var resultado = valor1 + valor2;
+        var mensagem = "A soma dos valores é: " + resultado;
+        return (resultado, mensagem);
+    }
+}
+
+
